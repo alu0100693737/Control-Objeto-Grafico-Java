2d0e8cd65d0afc797a76e55b2058506a50a9b3a0
alu0100693737 <alu0100693737@ull.edu.es>
2016-04-08 17:01:46 +0200

    Trabajando con los movimientos

--- ControlBola/src/controlBola/PanelBola.java
+++ ControlBola/src/controlBola/PanelBola.java
@@ -7,16 +7,49 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 
 public class PanelBola extends JPanel {
-	public PanelBola() {
+	
+	private final int RADIO = 50;
+	private final int POS_INICIO_X = 275;
+	private final int POS_INICIO_Y = 175;
+	private int diffXInicio;
+	private int diffYInicio;
+	private static int velocidad;
+	
+	public PanelBola(int vel) {
 		setBackground(Color.CYAN);	
+		diffXInicio = 0;
+		diffYInicio = 0;
+		velocidad = vel;
 	}
 	
-	public void paintComponent( Graphics g) {
-		super.paintComponent( g );
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		//cara
 		g.setColor(Color.RED);
-		g.fillOval( 275, 175, 50, 50);
+		g.fillOval(POS_INICIO_X + getDiffXInicio(), POS_INICIO_Y + getDiffYInicio(), RADIO, RADIO);
 	}
 	
-
+	public int getDiffXInicio() {
+		return diffXInicio;
+	}
+	
+	public int getDiffYInicio() {
+		return diffYInicio;
+	}
+	
+	//valor 0 para left 1 para right
+	public void setDiffXInicio(int val) {
+		diffXInicio += val * getVelocidad();
+	}
+	
+	//valor 0 para up, 1 para down
+	public void setDiffYInicio(int val) {
+		diffYInicio += val * getVelocidad();
+	}
+	
+	public static int getVelocidad() {
+		return velocidad;
+	}
+	
+	
 }
--- ControlBola/src/controlBola/PanelBotones.java
+++ ControlBola/src/controlBola/PanelBotones.java
@@ -4,9 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.TextField;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class PanelBotones extends JPanel {
 	
@@ -14,6 +17,8 @@ public class PanelBotones extends JPanel {
 	private JButton btnDown;
 	private JButton btnLeft;
 	private JButton btnRight;
+	private JTextField txtAutor;
+	
 	private static final int BUTTON_LOCATION_X = 250;  // location x 
 	private static final int BUTTON_LOCATION_Y = 50;   // location y 
 	private static final int BUTTON_SIZE_X = 100;      // size height
@@ -37,11 +42,15 @@ public class PanelBotones extends JPanel {
 				BUTTON_SIZE_X, BUTTON_SIZE_Y );
 		getbtnDown().setBounds(BUTTON_LOCATION_X, ESPACIO_VERTICAL + ESPACIO_VERTICAL
 				, BUTTON_SIZE_X, BUTTON_SIZE_Y );
-		
+		txtAutor = new JTextField("Autor: Iván García Campos");
+		getAutor().setHorizontalAlignment(JTextField.CENTER);
+		getAutor().setBounds(BUTTON_LOCATION_X-BUTTON_LOCATION_Y, ESPACIO_VERTICAL + ESPACIO_VERTICAL + ESPACIO_VERTICAL,
+				150 + ESPACIO_HORIZONTAL, ESPACIO_VERTICAL);
 		add(getbtnUp());
 		add(getbtnDown());
 		add(getbtnLeft());
 		add(getbtnRight());
+		add(getAutor());
 	}
 	
 	public JButton getbtnUp() {
@@ -58,5 +67,9 @@ public class PanelBotones extends JPanel {
 
 	public JButton getbtnRight() {
 		return btnRight;
+	}
+	
+	public JTextField getAutor() {
+		return txtAutor;
 	}
 }
