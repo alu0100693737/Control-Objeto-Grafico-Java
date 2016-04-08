f10939a013fbca64f4171fe7aa0ab34d4eb47d30
alu0100693737 <alu0100693737@ull.edu.es>
2016-04-08 13:57:50 +0200

    Trabajando con el panel botones

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a9/f03be1e680fd001514f8df242059e46b
@@ -0,0 +1,15 @@
+package controlBola;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+
+public class VentanaBola extends JFrame {
+	private PanelBola bola;
+	public VentanaBola() {
+		setLayout(new GridLayout(2, 1, 5, 5));
+		bola = new PanelBola();
+		add(bola);
+	}
+
+}
--- ControlBola/src/controlBola/VentanaBola.java
+++ ControlBola/src/controlBola/VentanaBola.java
@@ -6,10 +6,13 @@ import javax.swing.JFrame;
 
 public class VentanaBola extends JFrame {
 	private PanelBola bola;
+	private PanelBotones botones;
 	public VentanaBola() {
 		setLayout(new GridLayout(2, 1, 5, 5));
 		bola = new PanelBola();
+		botones = new PanelBotones();
 		add(bola);
+		add(botones);
 	}
 
 }
