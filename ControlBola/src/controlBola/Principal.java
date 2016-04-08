372eaebde3c1c7bcb487487268e41fb9c5a0f4ef
alu0100693737 <alu0100693737@ull.edu.es>
2016-04-08 14:46:58 +0200

    Panel botones terminado

--- ControlBola/src/controlBola/Principal.java
+++ ControlBola/src/controlBola/Principal.java
@@ -6,9 +6,10 @@ public class Principal {
 	public static void main(String[] args) {
 		VentanaBola prueba = new VentanaBola();
 		prueba.setTitle("TestCaracteristicasComunes");
-		prueba.setSize(500, 500);
+		prueba.setSize(600, 600);
 		prueba.setLocationRelativeTo(null); // Center the frame
 		prueba.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		prueba.setResizable(false);
 		prueba.setVisible(true);
 	}
 }
--- ControlBola/src/controlBola/VentanaBola.java
+++ ControlBola/src/controlBola/VentanaBola.java
@@ -1,5 +1,7 @@
 package controlBola;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JFrame;
@@ -8,11 +10,14 @@ public class VentanaBola extends JFrame {
 	private PanelBola bola;
 	private PanelBotones botones;
 	public VentanaBola() {
-		setLayout(new GridLayout(2, 1, 5, 5));
+		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		bola = new PanelBola();
 		botones = new PanelBotones();
+		bola.setPreferredSize(new Dimension(600, 400));
+		botones.setPreferredSize(new Dimension(600, 200));
 		add(bola);
 		add(botones);
+
 	}
 
 }
